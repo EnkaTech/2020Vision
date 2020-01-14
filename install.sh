7ec39ae196f1932b4fd326d876c960e073e939fd
berkan-sahin <tt.berkan092@gmail.com>
2020-01-14 22:21:28 +0300

    Finalized install script

--- install.sh
+++ install.sh
@@ -12,11 +12,12 @@ TARGET="/opt/2020vision/"
 SYSD_DIR="/lib/systemd/system/"
 
 if [[ -d $TARGET ]]; then
-    echo "$TARGET already exists as a directory. Replace it?(y/n): "
+    echo -n "$TARGET already exists as a directory. Replace it? [y/n]: "
     read -n 1 ans
     
     if [[ $ans == "y" ]] || [[ $ans == "Y" ]]; then
 	rm -r $TARGET
+	echo
     else exit
     fi
 fi
@@ -29,8 +30,12 @@ for file in *.py; do
     cp $file "${TARGET}${file}"
 done
 
-echo "Copying the service file to $TARGET"
+
+echo "Copying the service file to $SYSD_DIR"
 cp 2020vision.service "${SYSD_DIR}2020vision.service"
+
+echo "Changing permissions on the service file"
+sudo chmod 644 "${SYSD_DIR}2020vision.service"
 
 echo "Done!"
 echo "Read the instructions on README for enabling the service"
