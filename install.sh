642605139305668797e182accebbddaba57b04bd
Berkan Sahin <tt.berkan092@gmail.com>
2020-01-27 22:48:12 +0300

    Choose between server and client while installing

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ fi
 TARGET="/opt/2020vision/"
 SYSD_DIR="/lib/systemd/system/"
 
+if [[ -e "${SYSD_DIR}2020vision.service" ]]; then
+    echo "Stopping the service..."
+    systemctl stop 2020vision.service
+fi
+
 if [[ -d $TARGET ]]; then
     echo -n "$TARGET already exists as a directory. Replace it? [y/n]: "
     read -n 1 ans
@@ -24,12 +29,23 @@ fi
 
 
 mkdir $TARGET
+
+echo -n "Deploy client mode? [y/N]: "
+read -n 1 ans
+echo
+if [[ $ans == "y" ]] || [[ $ans == "Y" ]]; then
+    ln -s main_client.py main.py
+else ln -s main_server.py main.py
+fi
+
+
 echo "Copying python scripts to $TARGET"
 for file in *.py; do
-
     cp $file "${TARGET}${file}"
 done
 
+echo "Removing temprorary files"
+rm main.py
 
 echo "Copying the service file to $SYSD_DIR"
 cp 2020vision.service "${SYSD_DIR}2020vision.service"
